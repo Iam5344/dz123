d981e57f6afd68a3a6dfb16d9991e80874c91aab
Arthas <gleban228777@gmail.com>
2026-04-18 20:52:39 +0300

    Update Form1.cs

--- WinFormsApp7/Form1.cs
+++ WinFormsApp7/Form1.cs
@@ -1,62 +1,58 @@
+using System;
 using System.Diagnostics;
+using System.Windows.Forms;
 
-namespace WinFormsApp7;
-
-public partial class Form1 : Form
+namespace WinFormsApp7
 {
-    public Form1()
+    public partial class Form1 : Form
     {
-        InitializeComponent();
-        LoadProcesses();
-    }
-
-    private void LoadProcesses()
-    {
-        listBox1.Items.Clear();
-        foreach (Process p in Process.GetProcesses())
-            listBox1.Items.Add(p.ProcessName + " | PID: " + p.Id);
-    }
-
-    private void timer1_Tick(object sender, EventArgs e)
-    {
-        LoadProcesses();
-    }
-
-    private void numericUpDown1_ValueChanged(object sender, EventArgs e)
-    {
-        timer1.Interval = (int)(numericUpDown1.Value * 1000);
-    }
-
-    private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-    {
-        if (listBox1.SelectedItem == null) return;
-
-        string selected = listBox1.SelectedItem.ToString();
-        int pid = int.Parse(selected.Split("PID: ")[1]);
-
-        Process p = Process.GetProcessById(pid);
-        p.Refresh();
-
-        label1.Text = "PID: " + p.Id;
-        label2.Text = "Потоків: " + p.Threads.Count;
-        label3.Text = "Копій: " + Process.GetProcessesByName(p.ProcessName).Length;
-
-        try { label4.Text = "Старт: " + p.StartTime.ToString("HH:mm:ss"); }
-        catch { label4.Text = "Старт: недоступно"; }
-
-        try { label5.Text = "CPU: " + p.TotalProcessorTime.ToString(@"hh\:mm\:ss"); }
-        catch { label5.Text = "CPU: недоступно"; }
-
-        button1.Enabled = true;
-        button1.Tag = pid;
-    }
-
-    private void button1_Click(object sender, EventArgs e)
-    {
-        int pid = (int)button1.Tag;
-        if (MessageBox.Show("Завершити процес PID " + pid + "?", "Підтвердження",
-            MessageBoxButtons.YesNo) == DialogResult.Yes)
+        public Form1()
         {
+            InitializeComponent();
+            LoadProcesses();
+        }
+
+        private void LoadProcesses()
+        {
+            listBox1.Items.Clear();
+            Process[] processes = Process.GetProcesses();
+            foreach (Process p in processes)
+            {
+                listBox1.Items.Add(p.ProcessName + " PID: " + p.Id);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            LoadProcesses();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            timer1.Interval = (int)numericUpDown1.Value * 1000;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem == null) return;
+
+            string item = listBox1.SelectedItem.ToString();
+            int pid = int.Parse(item.Split("PID: ")[1]);
+            Process p = Process.GetProcessById(pid);
+
+            label1.Text = "PID: " + p.Id;
+            label2.Text = "РџРѕС‚РѕРєС–РІ: " + p.Threads.Count;
+            label3.Text = "РљРѕРїС–Р№: " + Process.GetProcessesByName(p.ProcessName).Length;
+            label4.Text = "РЎС‚Р°СЂС‚: " + p.StartTime;
+            label5.Text = "CPU: " + p.TotalProcessorTime;
+
+            button1.Tag = pid;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Tag == null) return;
+            int pid = (int)button1.Tag;
             Process.GetProcessById(pid).Kill();
             LoadProcesses();
         }
